3c1243465db3c2c664b902cbde0c147eebdde7e2
Vágó Rebeka <rebeka.vago19@gmail.com>
2022-03-29 21:40:31 +0200

    végre működik

--- hf/kepet_ker.js
+++ hf/kepet_ker.js
@@ -1,11 +1,33 @@
-const xhttp = new XMLHttpRequest();
-xhttp.onload = function(data){
-    if (data.target.status == 200) {
-        const valasz = data.target.response;
-        const kep = JSON.parse(valasz);
-        document.getElementById('kep').src = kep.file;
-        console.log(kep);
-    }
-};
-xhttp.open("GET", "https://aws.random.cat/meow");
-xhttp.send();
+function ujKep(){
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function(data){
+        if (data.target.status == 200) {
+            const valasz = data.target.response;
+            const kep = JSON.parse(valasz);
+            document.getElementById('kep').src = kep.file;
+            console.log(kep);
+        }
+    };
+    xhttp.open("GET", "https://aws.random.cat/meow");
+    xhttp.send();
+}
+// const data = "https://aws.random.cat/meow"
+// function ujKep(data) {
+//     if (data.target.status == 200) {
+//         const valasz = data.target.response;
+//         const kep = JSON.parse(valasz);
+//         document.getElementById('kep').src = kep.file;
+//     }
+//   };
+
+// const ujkep = new XMLHttpRequest();
+// function ujKep(data="https://aws.random.cat/meow"){
+//     if (data.target.status == 200) {
+//         const valasz = data.target.response;
+//         const kep = JSON.parse(valasz);
+//         document.getElementById('kep').src = kep.file;
+//         console.log(kep);
+//     }
+// };
+// ujkep.open("GET", "https://aws.random.cat/meow");
+// ujkep.send();
